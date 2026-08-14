a396d863d03d9b9a17d78f31f2e8f2274c7ddea1
conqueror1996 <urbanclay@users.noreply.github.com>
2026-08-14 14:15:06 +0530

    Restore PlayInHorse/BetInExchange88 login handlers

--- nan_dashboard.py
+++ nan_dashboard.py
@@ -375,6 +375,13 @@ def auto_login(site_key, username, password):
             headers=make_xhr_headers("application/x-www-form-urlencoded"),
             method="POST")
 
+    elif site_key in ("playinhorse.com", "betinexchange88.com"):
+        login_data = json.dumps({"username": username, "password": password}).encode()
+        login_req = urllib.request.Request(f"{base}/api2/v2/login",
+            data=login_data,
+            headers=make_xhr_headers("application/json"),
+            method="POST")
+
     elif site_key in ("cricash24.com", "spinmatch99.com", "spinjeet365.com"):
         login_data = urlencode({
             "email": username, "password": password,
